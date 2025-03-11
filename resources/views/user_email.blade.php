69a03abdb852ff3003b2a1cf7fb27b1030f91194
Nsengiyunva <kingbecks07@gmail.com>
2025-03-11 18:13:06 +0300

    adds the mail

---
+++ resources/views/user_email.blade.php
@@ -0,0 +1,12 @@
+<br/>
+Dear {{ $customer_details['name'] }}
+<br/>
+{{ $customer_details['content'] }}
+<br/>
+<br/><br/>
+
+<br/>
+We thank you for your interest in commercial forest farming<br/>
+<strong>NFA Administration</strong><br/>
+Tel: +256<br/>
+Email<br/>
--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::group([ 'middleware' => [ 'api', 'cors' ],'prefix' => 'auth' ], function ($router) {
-  Route::post('/test', [OtherController::class, "index" ] );   
+  Route::post('/test', [MainController::class, "sendmail" ] );   
   
   Route::post( "erb_loginUser", [EngineersController::class, 'loginUser'] );
   Route::post( "erb_storeUser", [EngineersController::class, 'storeUser'] );
