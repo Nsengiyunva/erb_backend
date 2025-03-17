a0ddf13ad45d37eea4112ea9336d2e9d1332fa90
Nsengiyunva <kingbecks07@gmail.com>
2025-03-17 14:31:17 +0300

    updates the erb api

--- app/Http/Controllers/OTPController.php
+++ app/Http/Controllers/OTPController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendMail;
-// use App\Mail\OTPMail;
+use App\Mail\OTPMail;
 use App\Models\ELicenceUser;
 use Illuminate\Support\Facades\Cache;
 
