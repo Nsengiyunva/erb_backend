42e86ff45d4ec94ba6b07f8473fe6e4fd166944c
Nsengiyunva <kingbecks07@gmail.com>
2025-03-17 14:30:15 +0300

    updates the erb api

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,6 @@ use App\Http\Controllers\OtherController;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
