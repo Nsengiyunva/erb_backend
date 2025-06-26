e4f984aaae35947e94e9bb160f09d9a9004db424
unknown <Nsengiyunva>
2025-06-26 21:40:24 +0300

    updates the app

--- app/Http/Controllers/EngineersController.php
+++ app/Http/Controllers/EngineersController.php
@@ -834,7 +834,7 @@ class EngineersController extends Controller
     }
 
     public function fetchAccountLicenses( $user_id ) {
-        $applications = ELicence::where('applicant_id', $user_id )->get();
+        $applications = ELicence::where('applicant_id', $user_id )->where('draft_type', 'COMPLETED')->get();
         return response()->json( [
             "success" => true,
             "records" => $applications
