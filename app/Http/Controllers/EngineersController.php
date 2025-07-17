c26b50b3a0a7429ebef04df3a2b09917804726d0
unknown <Nsengiyunva>
2025-07-17 14:39:46 +0300

    updates the erb backend

--- app/Http/Controllers/EngineersController.php
+++ app/Http/Controllers/EngineersController.php
@@ -604,13 +604,14 @@ class EngineersController extends Controller
 
     public function isEngineerAuthenticated( Request $request ) {
         $sql =  ELicenceUser::where( "email", $request->user_name )->where( "password", $request->password )->first();
-        // if( empty( $sql ) ) {
-        //     return response()->json([
-        //         'message' => 'Engineer was not found in the databaase and maybe not registered yet.'
-        //     ], 404);
-        // }
+        if( empty( $sql ) ) {
+            return response()->json([
+                'message' => 'Engineer was not found in the databaase and maybe not registered yet.'
+            ], 404);
+        }
         return response()->json( [
-            "result": $sql
+            "is_autheticated" => true,
+            "result" => $sql
         ] );
     }
 
