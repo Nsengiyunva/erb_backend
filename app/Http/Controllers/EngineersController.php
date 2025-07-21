af3a717ee3f53c4e197f61ef5f7d08bb204e06a4
unknown <Nsengiyunva>
2025-07-21 09:23:18 +0300

    updates the app

--- app/Http/Controllers/EngineersController.php
+++ app/Http/Controllers/EngineersController.php
@@ -871,7 +871,7 @@ class EngineersController extends Controller
         //     }
         // }    
         return response()->json( [
-            "success" => "we are testing"
+            "success" => $request->registered
         ] );   
     }
 
