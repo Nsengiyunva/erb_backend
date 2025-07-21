16160aa488d7feb58e2d0399bdeac15813e0a3a2
unknown <Nsengiyunva>
2025-07-21 09:20:51 +0300

    updates the app

--- app/Http/Controllers/EngineersController.php
+++ app/Http/Controllers/EngineersController.php
@@ -785,41 +785,45 @@ class EngineersController extends Controller
                 ] );
             }
             else {
-                $user = new ELicenceUser;
+                // $user = new ELicenceUser;
 
-                $user->type = $request->type;
-                $user->name = $request->name;
-                $user->first_name = $request->first_name;
-                $user->last_name = $request->last_name; //add
-                $user->surname = $request->surname;
-                $user->other_names = $request->other_names;
-                $user->telephone = $request->telephone;
-                $user->phone_no = $request->phone_no; //add
-                $user->origin_license_no = $request->origin_license_no; //add
-                $user->email = $request->email;
-                $user->birth_place = $request->birth_place;
-                $user->dob = $request->birth_date;
-                $user->gender = $request->gender;
-                $user->company_name = $request->company_name;
-                $user->address = $request->address;
-                $user->status = $request->status;
-                $user->user_type = $request->user_type;
-                $user->password = $request->password;
-                $user->country = $request->country;
-                $user->registered = $request->registered;
-                $user->category = $request->category;
-                $user->licence_no = $request->licence_no;
-                $user->user_picture = $request->user_picture;
+                // $user->type = $request->type;
+                // $user->name = $request->name;
+                // $user->first_name = $request->first_name;
+                // $user->last_name = $request->last_name; //add
+                // $user->surname = $request->surname;
+                // $user->other_names = $request->other_names;
+                // $user->telephone = $request->telephone;
+                // $user->phone_no = $request->phone_no; //add
+                // $user->origin_license_no = $request->origin_license_no; //add
+                // $user->email = $request->email;
+                // $user->birth_place = $request->birth_place;
+                // $user->dob = $request->birth_date;
+                // $user->gender = $request->gender;
+                // $user->company_name = $request->company_name;
+                // $user->address = $request->address;
+                // $user->status = $request->status;
+                // $user->user_type = $request->user_type;
+                // $user->password = $request->password;
+                // $user->country = $request->country;
+                // $user->registered = $request->registered;
+                // $user->category = $request->category;
+                // $user->licence_no = $request->licence_no;
+                // $user->user_picture = $request->user_picture;
 
-                $user->created_at = now();
-                $user->updated_at = now();
-                $user->save();
+                // $user->created_at = now();
+                // $user->updated_at = now();
+                // $user->save();
 
-                return response()->json([
-                    "success" => true,
-                    "result" => 0,
-                    "message" => "Account has been created successfully."
-                ]);
+                // return response()->json([
+                //     "success" => true,
+                //     "result" => 0,
+                //     "message" => "Account has been created successfully."
+                // ]);
+
+                return response()->json( [
+                    "success" => "we are testing"
+                ] );
             }
         }
         else {
